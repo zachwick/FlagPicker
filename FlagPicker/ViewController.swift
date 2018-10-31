38b406b4c1e91ca40bc02e762fb3da5b51ef4683
zachwick <zach@zachwick.com>
2018-10-30 21:51:52 -0400

    Initial Commit

---
+++ FlagPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlagPicker
+//
+//  Created by zach wick on 2018-10-30.
+//  Copyright © 2018 zach wick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
