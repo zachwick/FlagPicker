dcdf216a29becae3460f83e2a831783ec188e498
zachwick <zach@zachwick.com>
2018-10-30 22:40:36 -0400

    Code complete
    Signed-off-by: zachwick <zach@zachwick.com>

--- FlagPicker/ViewController.swift
+++ FlagPicker/ViewController.swift
@@ -35,7 +35,7 @@ class ViewController: UIViewController {
         askQuestion()
     }
 
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
         countries = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: countries) as! [String]
         correctAnswer = GKRandomSource.sharedRandom().nextInt(upperBound: 3)
         title = countries[correctAnswer].uppercased()
@@ -44,6 +44,21 @@ class ViewController: UIViewController {
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
     }
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue?", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+    }
+    
 }
 
